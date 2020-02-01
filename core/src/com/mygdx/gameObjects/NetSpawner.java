36095a352c062a492ec621890cc28495fbc8fbad
AconLight <w.mosienski@gmail.com>
2020-02-01 20:29:50 +0100

    enemy

--- core/src/com/mygdx/gameObjects/NetSpawner.java
+++ core/src/com/mygdx/gameObjects/NetSpawner.java
@@ -3,6 +3,8 @@ package com.mygdx.gameObjects;
 import boost.GameObject;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mygdx.networking.NetworkApi;
+import lgj.objects.Enemy;
+import java.util.Random;
 
 /**
  * Created by Wojciech on 2020-01-23.
@@ -25,5 +27,17 @@ public class NetSpawner extends GameObject {
     public void spawnAsMine() {
         test = new NetTest(true, "player" + NetworkApi.manager.myAddress.ip + NetworkApi.manager.myAddress.port);
         stage.addActor(test);
+        Random rand = new Random();
+        stage.addActor(new Enemy(true, "ddd" + NetworkApi.manager.myAddress.ip + NetworkApi.manager.myAddress.port,
+                rand.nextInt(3),
+                rand.nextInt(3),
+                rand.nextInt(3),
+                rand.nextInt(3),
+                rand.nextInt(4),
+                rand.nextInt(3),
+                rand.nextInt(4),
+                rand.nextInt(3),
+                rand.nextInt(4)
+        ));
     }
 }
