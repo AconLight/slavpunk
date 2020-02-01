3e8d2f576d7c50b0d6dd135b6050ef4212c500b0
Piotr Ste <pstefanski@gmail.com>
2020-02-01 21:22:33 +0100

    enemy fokders animations

--- core/src/lgj/objects/Enemy.java
+++ core/src/lgj/objects/Enemy.java
@@ -34,13 +34,12 @@ public class Enemy extends GameObject {
         this.legs = legs;
         this.legs_color = legs_color;
 
-        colorInit();
-
         posX = 800;
         posY = 260;
         scale = 10;
         velocity = 5;
 
+        colorInit();
         animationInit();
         changeColor();
 
@@ -63,32 +62,38 @@ public class Enemy extends GameObject {
         posX = 0;
         posY = 0;
 
-        head_ = AssetLoader.getAnimation("head" + head);
+        head_ = AssetLoader.getAnimation("enemy_head");
+        head_.chooseAnimation(Integer.toString(head));
         head_.setPosition(posX, posY);
         head_.setScale(scale);
         addActor(head_);
 
-        body_ = AssetLoader.getAnimation("body" + body);
+        body_ = AssetLoader.getAnimation("enemy_body");
+        body_.chooseAnimation(Integer.toString(body));
         body_.setPosition(posX, posY);
         body_.setScale(scale);
         addActor(body_);
 
-        renka_ = AssetLoader.getAnimation("renka" + renka);
+        renka_ = AssetLoader.getAnimation("enemy_renka");
+        renka_.chooseAnimation(Integer.toString(renka));
         renka_.setPosition(posX, posY);
         renka_.setScale(scale);
         addActor(renka_);
 
-        weapon_ = AssetLoader.getAnimation("weapon" + weapon);
+        weapon_ = AssetLoader.getAnimation("enemy_weapon");
+        weapon_.chooseAnimation(Integer.toString(weapon));
         weapon_.setPosition(posX, posY);
         weapon_.setScale(scale);
         addActor(weapon_);
 
-        eye_ = AssetLoader.getAnimation("eye" + eye);
+        eye_ = AssetLoader.getAnimation("enemy_eye");
+        eye_.chooseAnimation(Integer.toString(eye));
         eye_.setPosition(posX, posY);
         eye_.setScale(scale);
         addActor(eye_);
 
-        legs_ = AssetLoader.getAnimation("legs" + legs);
+        legs_ = AssetLoader.getAnimation("enemy_legs");
+        legs_.chooseAnimation(Integer.toString(legs));
         legs_.setPosition(posX, posY);
         legs_.setScale(scale);
         addActor(legs_);
@@ -98,7 +103,6 @@ public class Enemy extends GameObject {
 
 
     void changeColor(){
-
         eye_.setColor(colory.get(eye_color));
         weapon_.setColor(colory.get(weapon_color));
         legs_.setColor(colory.get(legs_color));
@@ -135,4 +139,8 @@ public class Enemy extends GameObject {
         posY = posY2;
     }
 
+    public void setVelocity(float velocity) {
+        this.velocity = velocity;
+    }
+    
 }
