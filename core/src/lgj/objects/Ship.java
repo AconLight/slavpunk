32e1f8953079c76bfc9faba11c60fec9baa1499c
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-02-02 11:13:39 +0100

    Healthbard ship

--- core/src/lgj/objects/Ship.java
+++ core/src/lgj/objects/Ship.java
@@ -3,16 +3,26 @@ package lgj.objects;
 import assets.AssetLoader;
 import boost.GameObject;
 import boost.SpriteObject;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 public class Ship extends GameObject {
 
     SpriteObject ship;
     boolean isMine;
     float posX, posY, scale;
+    int health, maxHealth;
+    Progress healthBar;
 
     public Ship(boolean isMine, String id) {
         super(1, id);
         this.isMine = isMine;
+
+        health = 100;
+        maxHealth = health;
+
+        healthBar = new Progress(200, 1750, 1900, 50, Color.RED);
 
         posX = 200;
         posY = 260;
@@ -38,5 +48,19 @@ public class Ship extends GameObject {
         float x = getX();
         float y = getY();
         setPosition((posX + x) / 2, (posY + y) / 2);
+    }
+
+    public void dealDamage() {
+        health--;
+        healthBar.getBar().setValue((float) health / (float) maxHealth);
+        if (health <= 0) {
+            // TODO gameover
+        }
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlfa) {
+        super.draw(batch, parentAlfa);
+        healthBar.getBar().draw(batch, parentAlfa);
     }
 }
